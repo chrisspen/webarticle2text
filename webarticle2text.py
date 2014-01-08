dfcb3b60c5e2282059ed7acd22b25d9ba3406d4e
chris <chris@coronis>
2014-01-07 19:01:29 -0500

    Updated version.

--- webarticle2text.py
+++ webarticle2text.py
@@ -24,7 +24,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 """
-VERSION = (1, 3, 0)
+VERSION = (1, 3, 1)
 __version__ = '.'.join(map(str, VERSION))
 import os
 import sys
